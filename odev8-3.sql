a57b4ddca4769cbc87b4bbafff26806abd954bec
Abdullah İlker Üğüdücü <105641416+abdullahilkeruguducu@users.noreply.github.com>
2022-10-17 18:08:50 +0300

    Create odev8-3.sql

---
+++ odev8-3.sql
@@ -0,0 +1,37 @@
+UPDATE employee
+SET name= 'tarkan',
+birthday = '1972-10-17',
+email = 'tarkan@tarkan.com'
+WHERE id = 17;
+
+------------
+
+UPDATE employee
+SET name= 'Eminem',
+birthday = '1972-10-17',
+email = 'eminem@eminem.com'
+WHERE id = 18;
+
+-----------
+
+UPDATE employee
+SET name= 'İlker',
+birthday = '1996-02-14',
+email = 'ilker@ilker.com'
+WHERE id = 19;
+
+-------------
+
+UPDATE employee
+SET name= 'Hasan',
+birthday = '1987-06-23',
+email = 'hasan@hasan.com'
+WHERE id = 20;
+
+----------
+
+UPDATE employee
+SET name= 'Mehmet',
+birthday = '1969-11-24',
+email = 'mehmet@mehmet.com'
+WHERE id = 21;
